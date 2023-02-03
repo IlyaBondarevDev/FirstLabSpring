fb18ecea43d951d70f09f06e06b63152eef06862
JamesCodi <james345345345345@gmail.com>
2023-02-03 12:46:35 +1000

    Commit

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,12 +3,12 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @RestController
 public class MainController {
-
     @GetMapping("/")
-    public String hello() {
-        return "Hello, Spring!";
-    }
+    public String hello() { return "Hello, Ilya Bondarev, today is " + LocalDate.now().toString(); }
 
 }
